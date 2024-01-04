7ae58aa9e8b6cae70dce4f6a823dd1a60852ce52
gkspranger <greg@spranger.us>
2024-01-04 15:26:56 -0500

    getting the basics ready

---
+++ generate-data.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+
+chars=abcdefghijklmnopqrstuzwxyz
+min=1
+max=100
+
+echo > data.txt
+for _ in {1..100000}; do
+  echo "${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1};$((min+RANDOM%(max-min))).$((RANDOM%99))" >> data.txt
+done
