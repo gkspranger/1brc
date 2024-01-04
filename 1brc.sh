d2abb74f112e327c1de7eebba2110813f84aedf3
gkspranger <greg@spranger.us>
2024-01-04 15:41:10 -0500

    getting a handle on using associative arrays

--- 1brc.sh
+++ 1brc.sh
@@ -1,3 +1,18 @@
 #!/usr/local/bin/bash
 
-echo "hello world"
+declare -A info
+
+echo $((info["nok"] + 0))
+
+info["ok"]=1
+echo $((info["ok"] + 0))
+
+
+
+
+
+
+# while IFS= read -r line; do
+#   arr=(${line//;/ })
+#   echo "${arr[0]} || ${arr[1]}"
+# done < data.txt
--- generate-data.sh
+++ generate-data.sh
@@ -5,6 +5,6 @@ min=1
 max=100
 
 echo > data.txt
-for _ in {1..100000}; do
+for _ in {1..1000}; do
   echo "${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1}-${chars:RANDOM%${#chars}:1};$((min+RANDOM%(max-min))).$((RANDOM%99))" >> data.txt
 done
